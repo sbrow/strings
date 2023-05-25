c05e2adce5f2e0323c4ee09d2bc1428268e61dc7
Spencer Brower <brower.spencer@gmail.com>
2023-05-25 15:53:05 -0400

    build: Prepared package for npm publishing.

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+export declare const startsWith: import("ts-toolbelt/out/Function/Curry").Curry<(needle: string, haystack: string) => boolean>;
+export declare const endsWith: import("ts-toolbelt/out/Function/Curry").Curry<(needle: string, haystack: string) => boolean>;
+/**
+ * Trims characters from the left side of a string.
+ */
+export declare const ltrim: import("ts-toolbelt/out/Function/Curry").Curry<(cutset: string, str: string) => string>;
---
+++ lib/utils.d.ts
@@ -0,0 +1,5 @@
+import type { curry as _curry, when as _when } from 'ramda';
+export declare const curry: typeof _curry;
+export declare const isEmpty: (str: string) => boolean;
+export declare const tail: (str: string) => string;
+export declare const when: typeof _when;
